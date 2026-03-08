aba5c394edc6a7a32679d6e5aa5f0b85b3eaa7f7
ssarkar313 <ssarkar313@gatech.edu>
2026-03-08 21:05:31 +0530

    Add static export config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // This tells Next.js to generate static HTML/CSS/JS
+  images: {
+    unoptimized: true, // Required for static export unless using a loader
+  },
 };
 
 export default nextConfig;
