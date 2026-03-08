c79e0857106f31b7e5fa98db7bbc83c5a1dce97b
ssarkar313 <ssarkar313@gatech.edu>
2026-03-08 21:32:26 +0530

    reverted to export method

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone', // This tells Next.js to generate static HTML/CSS/JS
+  output: 'export', // This tells Next.js to generate static HTML/CSS/JS
   images: {
     unoptimized: true, // Required for static export unless using a loader
   },
